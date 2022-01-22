7c9643ecec5150db8601083f90e111a6c84fa64d
Cartoonish Villain <jedijoe93@gmail.com>
2022-01-22 18:33:50 -0500

    First "release"

--- src/main/java/com/cartoonishvillain/cartoonishhorde/CartoonishHorde.java
+++ src/main/java/com/cartoonishvillain/cartoonishhorde/CartoonishHorde.java
@@ -21,49 +21,38 @@ import org.apache.logging.log4j.Logger;
 @Mod("cartoonishhorde")
 public class CartoonishHorde
 {
-    Horde horde;
+    //Horde horde;
     public static final String MOD_ID = "cartoonishhorde";
 
 
     public CartoonishHorde() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
-
-    }
-
-    private void doClientStuff(final FMLClientSetupEvent event) {
-
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        //Step 2 - Instantiate
-        horde = new Horde(event.getServer());
-        //This horde will consist of spiders, evokers, and creepers. Roughly equal quantities, but this is psuedo-randomized, so results may vary.
-        horde.setHordeData(new EntityHordeData<>(2, 1, 1, EntityType.GIANT, Giant.class));
-    }
-
-    @SubscribeEvent
-    public void onWorldTick(TickEvent.WorldTickEvent event) {
-        if(!event.world.isClientSide && horde != null) {
-            //Step 3 - Connect
-            horde.tick();
-        }
-    }
-
-    @SubscribeEvent
-    public void onPlayerJump(LivingEvent.LivingJumpEvent event) {
-        if(event.getEntityLiving() instanceof Player && !event.getEntityLiving().level.isClientSide && horde != null && !horde.getHordeActive()) {
-            //Step 4 - Start.
-            //This will start this test horde whenever it is not ongoing an a player jumps.
-            horde.SetUpHorde((ServerPlayer) event.getEntityLiving());
-        }
-    }
+    //Test code.
+//    @SubscribeEvent
+//    public void onServerStarting(ServerStartingEvent event) {
+//        //Step 2 - Instantiate
+//        horde = new Horde(event.getServer());
+//        //This horde will consist of spiders, evokers, and creepers. Roughly equal quantities, but this is psuedo-randomized, so results may vary.
+//        horde.setHordeData(new EntityHordeData<>(2, 1, 1, EntityType.GIANT, Giant.class));
+//    }
+//
+//    @SubscribeEvent
+//    public void onWorldTick(TickEvent.WorldTickEvent event) {
+//        if(!event.world.isClientSide && horde != null) {
+//            //Step 3 - Connect
+//            horde.tick();
+//        }
+//    }
+//
+//    @SubscribeEvent
+//    public void onPlayerJump(LivingEvent.LivingJumpEvent event) {
+//        if(event.getEntityLiving() instanceof Player && !event.getEntityLiving().level.isClientSide && horde != null && !horde.getHordeActive()) {
+//            //Step 4 - Start.
+//            //This will start this test horde whenever it is not ongoing an a player jumps.
+//            horde.SetUpHorde((ServerPlayer) event.getEntityLiving());
+//        }
+//    }
 }
